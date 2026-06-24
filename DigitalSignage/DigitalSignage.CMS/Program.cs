0abec4678750efce41286681fa279edd5ebfa526
DustBrosAndCo <dustbrosco@gmail.com>
2026-06-23 22:50:27 -0400

    2nd save

--- DigitalSignage/DigitalSignage.CMS/Program.cs
+++ DigitalSignage/DigitalSignage.CMS/Program.cs
@@ -1,25 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using DigitalSignage.CMS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapRazorComponents<DigitalSignage.CMS.Components.App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
